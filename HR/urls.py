59223b67e5983f2d6685e5efbb3c21f276e89276
Yasser-Sarhan <yassersarhan.python@gmail.com>
2024-11-25 11:37:59 +0200

    Add two functions to returning all posts and another one to ger one post and create two tempales one for all_posts and another one for post_details

--- HR/urls.py
+++ HR/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from post.views import post_list
+from post.views import post_list, post_detail
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blosg', post_list)
+    path('blog/', post_list),
+    path('blog/<id>', post_detail),
 ]
--- post/views.py
+++ post/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    pass
+    all_posts = Post.objects.all()
+    return render (request , 'all_posts.html', {'all_posts':all_posts})
 
-
-def post_detail(request):
-    pass
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render (request , 'post.html', {'post':post})
