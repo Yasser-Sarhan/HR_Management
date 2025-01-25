33f19d3fa920a8fa58be1a47489bcd219e78e11c
Yasser-Sarhan <yassersarhan.python@gmail.com>
2025-01-25 08:56:30 +0200

    Using class based view to CRUD on HR-Managment

--- post/urls.py
+++ post/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
    path('', views.post_list, name='post_list'),
    path('<int:id>', views.post_details, name='post_details'),
    path('create', views.post_create, name='post_create'),
-   path('<int:id>/edit', views.post_edit, name='post_edit')
+   path('<int:id>/edit', views.post_edit, name='post_edit'),
+   path('cbv/', views.PostList.as_view(), name='post_list_cbv'),
+   path('cbv/<int:pk>', views.PostDetail.as_view(), name='post_details_cbv'),
+   path('new/', views.CreatePost.as_view(), name= 'create_post')
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
+from django.views.generic import ListView, DetailView, CreateView
 
 # Create your views here.
 
@@ -31,3 +33,24 @@ def post_edit(request, id):
     else:
         form = PostForm(instance=post)
     return render(request, 'post/post_edit.html',{'form':form})
+
+
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'post/post_list.html'
+    context_object_name = 'all_posts'
+
+class PostDetail(DetailView):
+    pass
+    model =Post
+    template_name = 'post/post_detail.html'
+    context_object_name = 'post'
+
+class CreatePost(CreateView):
+    model = Post
+    template_name = 'post/post_create.html'
+    fields= '__all__'
+    success_url = reverse_lazy('success_url')
+    
