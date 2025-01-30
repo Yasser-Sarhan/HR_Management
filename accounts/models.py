8e9a4e9fe018122e14984b136844fcb969ee5fc0
Yasser-Sarhan <yassersarhan.python@gmail.com>
2025-01-30 11:47:39 +0200

    Create reciver between Profile and User

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from PIL import Image
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 
-def profile (models.model):
-    image = models.im
+class Profile (models.Model):
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    image = models.ImageField(upload_to='users/')
+    address = models.CharField(max_length=100)
+    phone_NO = models.CharField(max_length=13)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
